73af4aa5967d4ad4fbbf9da5cf9a7c78e40948a2
Pierre <pierrelemaire85@free.fr>
2021-06-06 23:24:06 +0200

    prise en compte https

--- configuration-https.py
+++ configuration-https.py
@@ -22,10 +22,8 @@ import subprocess
 
 """
     Tâches à effectuer : 
-        - Générer les certificats
         - Configurer apache pour le ssl
-        - Vérifier la conf Apache
-        - Configurer la redirection http -> https
+        - Configurer le groupe Diffie Helman
 """
 
 #Définition des constantes
@@ -37,9 +35,58 @@ LOG_FILE = "./configuration-https.log"
 try:
     logging.basicConfig(filename=LOG_FILE, format="%(asctime)s : %(levelname)s:%(message)s",
             level=logging.DEBUG)
-
+    logging.info("Début de la configuration https pour apache")
 except Exception as e:
     print("")
     raise e
+
+#Génération du groupe Diffie Hellmann afin d'améliorer la sécurité des certificats si le fichier n'existe pas
+
+try:
+    logging.info("Génération du fichier de groupe Diffie Hellmann /etc/ssl/certs/dhparam.pem")
+
+    if not os.path.isfile("/etc/ssl/certs/dhparam.pem"):
+        creation_dh_group = "openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048"
+        os.system(creation_dh_group)
+        logging.info("Le fichier a été créé avec succès")
+    else:
+        logging.info("Le fichier existe déjà, il n'a pas été modifié")
+
+except Exception as e:
+    logging.error("Echec de la création du fichier de groupe Diffie Hellmann")
+    logging.error(e)
+    raise e
+
+#Génération du fichier /etc/apache2/conf-available/ssl-params.conf
+
+try:
+    ssl_params = [
+            "SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH\n",
+            "SSLProtocol All -SSLv2 -SSLv3\n",
+            "SSLHonorCipherOrder On\n",
+            "Header always set Strict-Transport-Security \"max-age=63072000; includeSubdomains\"\n",
+            "Header always set X-Frame-Options DENY\n",
+            "Header always set X-Content-Type-Options nosniff\n",
+            "SSLCompression off\n",
+            "SSLSessionTickets Off\n",
+            "SSLUseStapling on\n",
+            "SSLStaplingCache \"shmcb:logs/stapling-cache(150000)\"\n",
+            "SSLOpenSSLConfCmd DHParameters \"/etc/ssl/certs/dhparam.pem\"\n"
+            ]
+
+    logging.info("Création du fichier /etc/apache2/conf-available/ssl-params.conf")
+    
+    if not os.path.isfile("/etc/apache2/conf-available/ssl-params.conf"):
+        with open("/etc/apache2/conf-available/ssl-params.conf", "w") as ssl_params_file:
+            ssl_params_file.writelines(ssl_params)
+            logging.info("Le fichier a été créé avec succès")
+    else:
+        logging.info("Le fichier ssl-params.conf existe déjà, il n'a pas été modifié")
+ 
+except Exception as e:
+    logging.error("Echec de la création du fichier apache /etc/apache2/conf-available/ssl-params.conf")
+    logging.error(e)
+    raise e
+
 
 
--- configuration-wordpress.py
+++ configuration-wordpress.py
@@ -22,6 +22,7 @@ import subprocess
 import validators 
 import apacheconfig
 import mariadb
+
 """
     Tâches à effectuer : 
         - lire le nom et dns du serveur wordpress
@@ -29,23 +30,24 @@ import mariadb
         - desactiver le site apache par défaut
         - activer le site apache wordpress
         - créer et remplir le fichier /etc/wordpress/config-MONDOMAINE.php
-        - créer et remplir le fichier wp.sql qui va créer la base du site Wordpress dans MariaDB
-        - créer la base SQL définie précedemment dans MariaDB 
+        - créer la base SQL dans MariaDB 
+        - mettre en place 
 
 """
 
-#Définition des constantes
+#Définition des constantes et variables
 
 LOG_FILE = "./configuration-wordpress.log"
-APACHE_FILE = "/etc/apache2/sites-available/wp.conf"
-DB_NAME = "wordpress"
-DB_USER = "wordpress"
-DB_PASSWORD = "password"
-DB_HOST = "localhost"
-WP_CONTENT_DIR = "/var/lib/wordpress/wp-content"
-SERVER_NAME = "wordpress.local"
-SERVER_ADMIN = "mail@mail"
+db_name = "wordpress"
+dbuser = "wordpress"
+db_password = "password"
+db_host = "localhost"
+WP_ROOT_DIR = "/usr/share/wordpress/"
+WP_CONTENT_DIR = "/var/lib/wordpress/wp-content/"
+server_name = "wordpress.local"
+server_admin = "mail@mail"
 
+MARIADB_ID = {"host": "localhost", "user": "root", "passwd": "OpenClassrooms21"}
 #Configuration du fichier de log
 
 try:
@@ -61,38 +63,34 @@ except Exception as e:
 try:
     
     logging.info("Lecture des informations utilisateur")
-    SERVER_NAME = input("Saisir le nom de domain complet (avec www. si besoin)\n")
-    print(SERVER_NAME) 
 
-    while not validators.domain(SERVER_NAME):
-        SERVER_NAME = input("Veuillez saisir un nom de domain correct\n")
+    server_name = input("Saisir le nom de domain complet (avec www. si besoin)\n")
+    #Je répète la saisie tant que le nom du serveur n'est pas un nom de domaine valide
+    while not validators.domain(server_name):
+        server_name = input("Veuillez saisir un nom de domain correct\n")
+
+    logging.info("Nom de domaine saisi : {}".format(server_name))
     
-    logging.info("Nom de domaine saisi : {}".format(SERVER_NAME))
-    
-    DB_NAME = input("Saisir le nom de la base de données [{}]:\n".format(DB_NAME))                          or "wordpress"
-    DB_USER = input("Saisir l'utilisateur de la base de données [{}]:\n".format(DB_USER))                   or "wordpress"
-    DB_PASSWORD = input("Saisir le mot de passe de la base de données [{}]:\n".format(DB_PASSWORD))         or "password"
-    DB_HOST = input("Saisir l'adresse de la base de données [{}]:\n".format(DB_HOST))                          or "localhost"
-    WP_CONTENT_DIR = input("Saisir le répertoire de contenu wordpress [{}]:\n".format(
-        WP_CONTENT_DIR)) or "/var/lib/wordpress/wp-content"
-    SERVER_ADMIN = input("Saisir l'adresse mail de l'administrateur [{}]:\n".format(SERVER_ADMIN))          or "mail@mail"
-
-
+    db_name = input("Saisir le nom de la base de données [{}]:\n".format(db_name))                          or "wordpress"
+    dbuser = input("Saisir l'utilisateur de la base de données [{}]:\n".format(dbuser))                   or "wordpress"
+    db_password = input("Saisir le mot de passe de la base de données [{}]:\n".format(db_password))         or "password"
+    db_host = input("Saisir l'adresse de la base de données [{}]:\n".format(db_host))                          or "localhost"
+    server_admin = input("Saisir l'adresse mail de l'administrateur [{}]:\n".format(server_admin))          or "mail@mail"
 
 except Exception as e:
     logging.error("Echec de la lecture des informations utilisateur")
     logging.error(e)
     raise e
 
-#Création du fichier /etc/apache2/sites-available/wp.conf
+#Création du fichier /etc/apache2/sites-available/'server_name'.conf
 
 try:
 
     apache_conf = [
         "<VirtualHost *:80>\n",
-        "\tServerName {}\n".format(SERVER_NAME),
-        "\tServerAdmin {}\n".format(SERVER_ADMIN),
-        "\tDocumentRoot /usr/share/wordpress\n",
+        "\tServerName {}\n".format(server_name),
+        "\tServerAdmin {}\n".format(server_admin),
+        "\tDocumentRoot {}\n".format(WP_ROOT_DIR),
         "\tAlias /wp-content {}\n".format(WP_CONTENT_DIR), 
         "\tErrorLog ${APACHE_LOG_DIR}/error.log\n",
         "\tCustomLog ${APACHE_LOG_DIR}/access.log combined\n",
@@ -109,12 +107,12 @@ try:
         "</VirtualHost>\n"
         ]
 
-    with open("/etc/apache2/sites-available/wp.conf", "w") as apache_file:
+    with open("/etc/apache2/sites-available/{}.conf".format(server_name), "w") as apache_file:
         apache_file.writelines(apache_conf)
-        logging.info("Le fichier /etc/apache2/sites-available/wp.conf a été créé avec succès")
+        logging.info("Le fichier /etc/apache2/sites-available/{}.conf a été créé avec succès".format(server_name))
 
 except Exception as e:
-    logging.error("Echec de la création du fichier wp.conf")
+    logging.error("Echec de la création du fichier {}.conf".format(server_name))
     logging.error(e)
     raise e
 
@@ -131,22 +129,22 @@ except Exception as e:
     logging.error(e)
     raise e
 
-#Création du fichier PHP /etc/wordpress/config-SERVER_NAME.php
+#Création du fichier PHP /etc/wordpress/config-server_name.php
 
 try:
     php_conf = [
             "<?php\n",
-            "define('DB_NAME','{}');\n".format(DB_NAME),
-            "define('DB_USER','{}');\n".format(DB_USER),
-            "define('DB_PASSWORD','{}');\n".format(DB_PASSWORD),
-            "define('DB_HOST','{}');\n".format(DB_HOST),
+            "define('DB_NAME','{}');\n".format(db_name),
+            "define('DB_USER','{}');\n".format(dbuser),
+            "define('DB_PASSWORD','{}');\n".format(db_password),
+            "define('DB_HOST','{}');\n".format(db_host),
             "define('WP_CONTENT_DIR','{}');\n".format(WP_CONTENT_DIR),
             "?>\n"
             ]
     
-    with open("/etc/wordpress/config-{}.php".format(SERVER_NAME), "w") as config_file_php:
+    with open("/etc/wordpress/config-{}.php".format(server_name), "w") as config_file_php:
         config_file_php.writelines(php_conf)
-        logging.info("Le fichier /etc/wordpress/config-{}.php a été créé avec succès".format(SERVER_NAME))
+        logging.info("Le fichier /etc/wordpress/config-{}.php a été créé avec succès".format(server_name))
 
 except Exception as e:
     logging.error("Echec de la création du fichier de configuration php")
@@ -156,17 +154,36 @@ except Exception as e:
 
 #Création de la base dans MariaDB
 
+#Définition d'une fonction qui indique l'existance d'une base de donnée dans mariadb
+
+def existe_base(nom_base, identifiants):
+    try:
+        with mariadb.connect(host = identifiants["host"],
+                user =identifiants["user"],
+                passwd = identifiants["passwd"]) as client_db:
+            
+            curseur_db = client_db.cursor()
+            curseur_db.execute("SHOW DATABASES")
+            for x in curseur_db:
+                if x[0] == nom_base:
+                    return True
+            
+            return False
+
+    except Exception as e:
+        raise e
+
 #Définition d'une fonction qui crée/remplace une base de donnée dans mariadb
 def creation_base(dico_base):
     try:
         with mariadb.connect(host = "localhost",
                 user = "root",
                 passwd = "OpenClassrooms21") as client_db:
-            sql_create_cmd = "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON {}.* TO {}@{} IDENTIFIED BY '{}'".format(dico_base["DB_NAME"], dico_base["DB_USER"], dico_base["DB_HOST"], dico_base["DB_PASSWORD"])
+            sql_create_cmd = "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON {}.* TO {}@{} IDENTIFIED BY '{}'".format(dico_base["db_name"], dico_base["dbuser"], dico_base["db_host"], dico_base["db_password"])
 
             curseur_db = client_db.cursor()
-            curseur_db.execute("DROP DATABASE IF EXISTS {}".format(dico_base["DB_NAME"]))
-            curseur_db.execute("CREATE DATABASE {}".format(dico_base["DB_NAME"]))
+            curseur_db.execute("DROP DATABASE IF EXISTS {}".format(dico_base["db_name"]))
+            curseur_db.execute("CREATE DATABASE {}".format(dico_base["db_name"]))
             curseur_db.execute(sql_create_cmd)
             curseur_db.execute("FLUSH PRIVILEGES")
     
@@ -174,37 +191,105 @@ def creation_base(dico_base):
         logging.error("Echec de la création de la base de données SQL dans MariaDB")
         raise e
 
+#Création de la base de données SQL
 try:
-    sql_identifiants = {"DB_NAME": DB_NAME, "DB_USER": DB_USER, "DB_HOST": DB_HOST, "DB_PASSWORD": DB_PASSWORD}
-        
+    sql_identifiants = {"db_name": db_name, "dbuser": dbuser, "db_host": db_host, "db_password": db_password}
+
     client_db = mariadb.connect(host = "localhost",
                                 user = "root",
                                 passwd = "OpenClassrooms21")
 
     curseur_db = client_db.cursor()
     curseur_db.execute("SHOW DATABASES")
-    for x in curseur_db:
 
-        if x[0] == DB_NAME:
-            print("La base {} existe déjà, souhaitez-vous continuer quand même et écraser la base existante ? [O/Y]/n".format(
+    if existe_base(db_name, MARIADB_ID):
+        print("La base {} existe déjà, souhaitez-vous continuer quand même et écraser la base existante ? [O/Y]/n".format(
                 x[0]))
-            choix_erreur_db = input("") or "O"
-            print(choix_erreur_db) 
-            if choix_erreur_db == "O" or choix_erreur_db == "Y":
-                client_db.close()
-                print("OK on est des fous on supprime tout !")
-                logging.info("La base '{}' existe déjà, elle va être réinitialisée".format(DB_NAME))
-                creation_base(sql_identifiants)
-                logging.info("La base '{}' a été réinitialisée".format(DB_NAME))
-            else:
-                print("Echec de création de la base, la base existe déjà.")
-                logging.error("Echec de la création de la base SQL, la base existe déjà.")
-                exit(os.EX_SOFTWARE)
-
+        choix_erreur_db = input("") or "O"
+        print(choix_erreur_db) 
+        if choix_erreur_db == "O" or choix_erreur_db == "Y":
+            client_db.close()
+            print("OK on est des fous on supprime tout !")
+            logging.info("La base '{}' existe déjà, elle va être réinitialisée".format(db_name))
+            creation_base(sql_identifiants)
+            logging.info("La base '{}' a été réinitialisée".format(db_name))
         else:
-            pass
+            print("Echec de création de la base, la base existe déjà.")
+            logging.error("Echec de la création de la base SQL, la base existe déjà.")
+            exit(os.EX_SOFTWARE)
+
+    else:
+        creation_base(sql_identifiants)
 
 except Exception as e:
     logging.error("Echec de la création de la base de donnée SQL dans MariaDB")
     logging.error(e)
     raise e   
+
+#Création des certificats ssl
+try:
+    creation_cert = "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/{}-selfsigned.key -out /etc/ssl/certs/{}-selfsigned.crt".format(server_name, server_name)
+    
+    creation_dh_group = "openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048"
+    
+    print("Création des certificats auto-signés")
+    
+    os.system(creation_cert)
+#    os.system(creation_dh_group)
+    logging.info("Certificats auto-signés créés avec succès")
+
+except Exception as e:
+    logging.error("Echec de la création des certificats")
+    logging.error(e)
+    raise e
+#Configuration d'Apache pour HTTPS
+try:
+    apache_https_conf = [
+            "<IfModule mod_ssl.c>\n",
+            "<VirtualHost *:443>\n",
+            "\tServerName {}\n".format(server_name),
+            "\tServerAdmin {}\n".format(server_admin),
+            "\tDocumentRoot {}\n".format(WP_ROOT_DIR),
+            "\tAlias /wp-content {}\n".format(WP_CONTENT_DIR), 
+            "\tErrorLog ${APACHE_LOG_DIR}/error.log\n",
+            "\tCustomLog ${APACHE_LOG_DIR}/access.log combined\n",
+            "\tSSLEngine on\n",
+            "\tSSLCertificateFile   /etc/ssl/certs/{}-selfsigned.crt\n".format(server_name),
+            "\tSSLCertificateKeyFile    /etc/ssl/private/{}-selfsigned.key\n".format(server_name),
+            "\t<FilesMatch \"\.(cgi|shtml|phtml|php)$\">\n",
+            "\t\tSSLOptions +StdEnvVars\n",
+            "\t</FilesMatch>\n",
+            "\t<Directory /usr/share/wordpress>\n",
+            "\t\tOptions FollowSymLinks\n",
+            "\t\tAllowOverride Limit Options FileInfo\n",
+            "\t\tDirectoryIndex index.php\n",
+            "\t\tRequire all granted\n",
+            "\t</Directory>\n",
+            "\t<Directory {}>\n".format(WP_CONTENT_DIR),
+            "\t\tOptions FollowSymLinks\n",
+            "\t\tRequire all granted\n",
+            "\t</Directory>\n",
+            "</VirtualHost>\n",
+            "</Ifmodule>\n",
+            "<VirtualHost *:80>\n",
+            "\tServerName {}\n".format(server_name),
+            "\tRedirect permanent / https://{}\n".format(server_name),
+            "</VirtualHost>\n"
+        ]
+
+    with open("/etc/apache2/sites-available/{}.conf".format(server_name), "w") as apache_https_file:
+        apache_https_file.writelines(apache_https_conf)
+        logging.info("Le fichier /etc/apache2/sites-available/{}.conf a été modifié avec succès".format(server_name))
+    os.system("a2enmod ssl")
+    os.system("a2enmod headers")
+    os.system("a2enconf ssl-params")
+    os.system("apache2ctl configtest")
+    os.system("systemctl restart apache2.service")
+
+
+
+
+except Exception as e:
+    logging.error("Echec de la configuration HTTPS d'Apache")
+    logging.error(e)
+    raise e
